8efbc8e2984377a9d281a21de38d1c0b42811858
jjijju0918 <112642622+jjijju0918@users.noreply.github.com>
2023-01-09 20:39:24 +0900

    JS__last scroll menu

--- main.js
+++ main.js
@@ -92,3 +92,34 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+// 1. 모든섹션 요소들과 메뉴아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰함
+// 3. 보여지는 섹션에 해당하는 메뉴아이템을 활성화 시킨다.
+
+const sectionIds = [
+    '#home', 
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonials',
+    '#contact',
+];
+
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+const observerOptions = {
+    root:null,
+    rootMargin: '0px',
+    threshold: 0.3,
+}
+
+const observerCallback = (entries,observer) => {
+    entries.forEach(entry => {
+        console.log(entry.target);
+    })
+}
+
+const observer = new IntersectionObserver(observerCallback,observerOptions);
+sections.forEach(section => observer.observe(section));
