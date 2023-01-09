5aae131d1c1bda5febe9ddf2435de639637e1c99
jjijju0918 <112642622+jjijju0918@users.noreply.github.com>
2023-01-09 15:20:28 +0900

    JS__Scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove("narbar__dark");
    }
 });
+
+//Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(e.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
