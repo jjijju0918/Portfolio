4f6993a959390bf82876f6c20d2b28eba569e85f
jjijju0918 <112642622+jjijju0918@users.noreply.github.com>
2023-01-09 18:39:28 +0900

    JS__Project animation

--- main.js
+++ main.js
@@ -50,6 +50,29 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter; 
+    if(filter == null) {
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+    projects.forEach((project) => {
+        if(filter === '*' || filter === project.dataset.type){
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+
+    setTimeout (() => {
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
