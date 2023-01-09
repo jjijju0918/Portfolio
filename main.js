44a7379c5a4245d278c87b0eee3efa4122d933e8
jjijju0918 <112642622+jjijju0918@users.noreply.github.com>
2023-01-09 19:25:24 +0900

    JS__Navbar toggle button

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ navbarMenu.addEventListener('click', (e) => {
     if(link == null) {
         return;
     }
-   scrollIntoView(link);
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 //Handle click on "contact me" button on home
